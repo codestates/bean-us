96e45a4b10ba8dc8541f9292d9b84187d716a473
jortier <jwon1818@daum.net>
2021-11-10 21:01:10 +0900

    Create model userBean
    유저와 원두의 조인테이블 생성

---
+++ server-bean/models/userbean.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class userBean extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  userBean.init({
+    userId: DataTypes.STRING,
+    beanId: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'userBean',
+  });
+  return userBean;
+};
