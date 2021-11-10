947584059f8c7f529de25057bab810aa4ab366ee
yeji <shfks1212@naver.com>
2021-11-10 10:43:22 +0900

    Set Front end
    프론트엔드 라우트, 컴포넌트, db, network, pages 기본 세팅

---
+++ client-bean/src/network/http.js
@@ -0,0 +1,7 @@
+// import axios from 'axios';
+
+// const http = process.env.REACT_APP_HTTPURL;
+
+// export function signup(userId, email, password) {
+//   return axios.post(http, { userId, email, password }, { 'Content-Type': 'application/json', withCredentials: true });
+// }
---
+++ client-bean/src/pages/MyPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MyPage = (props) => <div>Mypage</div>;
+
+export default MyPage;
