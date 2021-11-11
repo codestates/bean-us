dc08370969f65264f60bd50d66d31d6e61f4664d
song-code21 <wkcjf131313@gmail.com>
2021-11-12 08:22:03 +0900

    create sidebar
    스크롤 이동에 따른 위치표시 사이드바 기능 추가

--- client-bean/src/pages/Main.js
+++ client-bean/src/pages/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import {useLocation} from 'react-router-dom';
+import SideBar from '../components/MainPage/SideBar';
 import Section1 from '../components/MainPage/Section/Section1';
 import Section2 from '../components/MainPage/Section/Section2';
 import Section3 from '../components/MainPage/Section/Section3';
@@ -18,6 +19,7 @@ export default function Main () {
   const {scrollY} = useScroll();
   return (
     <MainContainer>
+        <SideBar scrollY={scrollY}/>
         <Section1 scrollY={scrollY}/>
         <Section2 scrollY={scrollY}/>
         <Section3 scrollY={scrollY}/>
