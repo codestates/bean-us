9abef5789880acfeef8f180adf19c98843d6ae77
jortier <jwon1818@daum.net>
2021-11-11 13:19:28 +0900

    Create filter-beans
    검색 조건에 따라 원두 목록을 응답 구현

--- server-bean/controller/beanController.js
+++ server-bean/controller/beanController.js
@@ -1,16 +1,66 @@
 const {beanInfo, userBean, sequelize} = require('./../models')
+const {Op, fn, col} = require('sequelize');
 
 module.exports = {
   allBeans:  (req, res) => {
     beanInfo.findAll({
       subQuery: false,
       raw: true,
-      // attributes: ['beanId', 'beanName', 'origin', 'fragrance', 'acidity', 'sweetness', 'bitterness', 'body', 'beanImage', ['description', 'desc']],
-      // attributes: [[sequelize.fn('COUNT', sequelize.col('userBeans.userId'), 'likeCount')]],
-      attributes: ['beanId', 'beanName', 'origin', 'fragrance', 'acidity', 'sweetness', 'bitterness', 'body', 'beanImage', ['description', 'desc'], [sequelize.fn('COUNT', sequelize.col('userBeans.userId')), 'likeCount']],
+      attributes: [
+        'beanId', 'beanName', 'origin', 'fragrance',
+        'acidity', 'sweetness', 'bitterness', 'body',
+        'beanImage', ['description', 'desc'],
+        [fn('COUNT', col('userBeans.userId')), 'likeCount']
+      ],
       include: [
         {model: userBean, required: false, attributes: []}
       ],
+      group: [
+        'beanId', 'beanName', 'origin', 'fragrance',
+        'acidity', 'sweetness', 'bitterness', 'body',
+        'beanImage', 'desc'
+      ],
+      order: [['beanId', 'asc']],
+
+    }).then(result => {
+      res.status(200).json({
+        message: 'success',
+        beanList: result,
+      });
+    });
+  },
+
+  filterBeans: (req, res) => {
+    const params = req.query;
+    const where = {};
+
+    for(let param in params){
+      if(param === 'bean' && params['bean'] !== ''){
+        where['beanName'] = {[Op.like] : `%${params[param]}%`}
+      }else{
+        if(params[param] !== ''){
+            where[param] = {
+              [Op.or]: params[param].split(',').map(item => {
+                return Number(item)
+              })
+            };
+        }
+      }
+    }
+
+    beanInfo.findAll({
+      subQuery: false,
+      raw: true,
+      attributes: [
+        'beanId', 'beanName', 'origin', 'fragrance',
+        'acidity', 'sweetness', 'bitterness', 'body',
+        'beanImage', ['description', 'desc'],
+        [fn('COUNT', col('userBeans.userId')), 'likeCount']
+      ],
+      include: [
+        {model: userBean, required: false, attributes: []}
+      ],
+      where: where,
       group: ['beanId', 'beanName', 'origin', 'fragrance', 'acidity', 'sweetness', 'bitterness', 'body', 'beanImage', 'desc'],
       order: [['beanId', 'asc']],
 
