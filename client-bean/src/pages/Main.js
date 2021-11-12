8f832b84ed5fe13684b8cf3f13705190624cc924
박예찬 <jkyyc3@gmail.com>
2021-11-12 09:48:21 +0900

    Put import
    빠진 import 문을 추가

--- client-bean/src/pages/Main.js
+++ client-bean/src/pages/Main.js
@@ -1,5 +1,3 @@
-/*eslint-disable no-unused-vars*/
-
 import React from 'react';
 // import {useLocation} from 'react-router-dom';
 import SideBar from '../components/MainPage/SideBar';
@@ -9,6 +7,7 @@ import Section3 from '../components/MainPage/Section/Section3';
 import styled from 'styled-components';
 import Footer from '../components/MainPage/Footer/Footer';
 import useScroll from '../components/MainPage/useScroll';
+import SignModal from '../components/signModal/SignModal';
 
 const MainContainer = styled.div`
   display: flex;
@@ -18,17 +17,17 @@ const MainContainer = styled.div`
 `;
 
 export default function Main({ isLogin, loginHandler, renderModal, modalHandler }) {
-  const {scrollY} = useScroll();
+  const { scrollY } = useScroll();
   return (
     <MainContainer>
-        <SideBar scrollY={scrollY} />
-        <Section1 scrollY={scrollY} isLogin={isLogin} modalHandler={modalHandler}/>
-        <Section2 scrollY={scrollY}/>
-        <Section3 scrollY={scrollY}/>
-        <Footer />
-        {renderModal ? (
-          <SignModal isLogin={isLogin} loginHandler={loginHandler} modalHandler={modalHandler} />
-        ) : null}
+      <SideBar scrollY={scrollY} />
+      <Section1 scrollY={scrollY} isLogin={isLogin} modalHandler={modalHandler} />
+      <Section2 scrollY={scrollY} />
+      <Section3 scrollY={scrollY} />
+      <Footer />
+      {renderModal ? (
+        <SignModal isLogin={isLogin} loginHandler={loginHandler} modalHandler={modalHandler} />
+      ) : null}
     </MainContainer>
   );
 }
