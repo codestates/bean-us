8dc69d1a7b6ee0450a4acf72284eb2d63b8e6f61
박예찬 <jkyyc3@gmail.com>
2021-11-13 01:13:09 +0900

    Fix bug
    잘못된 오류 수정

--- server-bean/router/beanRouter.js
+++ server-bean/router/beanRouter.js
@@ -1,11 +1,10 @@
-const controller = require('./../controller/beanController');
-const express = require('express');
+const controller = require("./../controller/beanController");
+const express = require("express");
 const router = express.Router();
 
-
-router.get('/all-beans', controller.allBeans);
-router.get('/filter-beans', controller.filterBeans);
-router.get('/', controller.findBeanPost);
-router.post('/like', controller.beanLike);
+router.get("/all-beans", controller.allBeans);
+router.get("/filter-beans", controller.filterBeans);
+router.get("/", controller.findBeanPost);
+router.post("/like", controller.beanLike);
 
 module.exports = router;
