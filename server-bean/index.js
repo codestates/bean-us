f796dea8e69f1fa9b9860a8be0f38002b6b23437
jortier <jwon1818@daum.net>
2021-11-14 20:51:07 +0900

    edit server index.js

--- server-bean/index.js
+++ server-bean/index.js
@@ -22,9 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    sameSite: 'None',
+    secure: true
   })
 );
 
