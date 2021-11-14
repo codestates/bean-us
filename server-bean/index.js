0f73f6ead2fd46d2ede47912e1211f7aba55cc7d
jortier <jwon1818@daum.net>
2021-11-14 15:48:55 +0900

    set sameSite

--- server-bean/index.js
+++ server-bean/index.js
@@ -25,7 +25,7 @@ app.use(
     origin: ['http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    sameSite: 'None'
+    sameSite: 'Lax',
   })
 );
 
