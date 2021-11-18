697749cd6c10df0093010b30b172e8c271e019b6
jortier <jwon1818@daum.net>
2021-11-18 10:43:29 +0900

    edit image upload

--- client-bean/src/network/postEdit/https.js
+++ client-bean/src/network/postEdit/https.js
@@ -6,8 +6,8 @@ axios.defaults.withCredentials = true;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 //게시글 수정
-export const putPosts = (postId, data) => {
-  return axios.put(`${https}/posts`, {...data, postId})
+export const putPosts = (data) => {
+  return axios.put(`${https}/posts`, data)
 }
 
 
